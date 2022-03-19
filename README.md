fbeaec22ea13636904acb577fa1dfe9e3e8fd2c9
happylie <happylie@hanmail.net>
2022-03-19 22:35:46 +0900

    Tool Update

--- README.md
+++ README.md
@@ -9,6 +9,158 @@
 - 매주 로또 당첨 번호 발표날 신규 회차 업데이트 예정
 - https://happylie.tistory.com/97
 
+## 로또 당첨 번호 Tool 
+### 설치 방법
+1. Git Clone
+```
+$ git clone https://github.com/happylie/lotto_data.git
+```
+2. Requirements 설치
+```
+$ pip install -r requirements.txt
+```
+### 실행 방법
+1. Help
+```
+$ python lotto_data.py -h
+usage: lotto [-h] [-d DB_PATH] [-i] [-s] [-t [TOP]] [-c [CUSTOM]] [-sr [SROUND]] [-er [EROUND]] [-exb] [-v]
+
+로또 최신 당첨번호 입력 및 통계 Tool
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DB_PATH, --db DB_PATH
+                        로또 당첨번호 DataBase Path
+  -i, --insert          최신 로또 당첨번호 DataBase Insert
+  -s, --stat            로또 당첨번호 통계
+  -t [TOP], --top [TOP]
+                        로또 당첨번호 전체 top 통계
+  -c [CUSTOM], --custom [CUSTOM]
+                        로또 당첨번호 Custom top 통계
+  -sr [SROUND], --sround [SROUND]
+                        로또 당첨번호 top 통계
+  -er [EROUND], --eround [EROUND]
+                        로또 당첨번호 top 통계
+  -exb, --ex_bonus      보너스볼 제외
+  -v, --version         show program's version number and exit
+```
+2. 최신 로또 데이터 등록
+```
+$ python ex_lotto.py -i                    
+정상적으로 1007회 로또 당첨 번호를 데이터베이스에 등록하였습니다.
+```
+3. 전체 당첨번호 통계 확인
+```
+$ python lotto_data.py -s
+### 1회 ~ 1007회 전체 통계 ###
+43번 : 180개
+1번 : 172개
+... 이하 생략 ...
+```
+4. 전체 당첨번호 보너스볼 제외 통계 확인
+```
+$ python lotto_data.py -s -exb
+### 1회 ~ 1007회 보너스볼 제외 통계 ###
+34번 : 152개
+18번 : 148개
+... 이하 생략 ...
+```
+5. 당첨번호 Top 통계 확인
+```
+$ python lotto_data.py -t
+### 1회 ~ 1007회 Top 6 전체 통계 ###
+43번 : 180개
+1번 : 172개
+27번 : 172개
+34번 : 171개
+17번 : 171개
+13번 : 171개
+
+$ python lotto_data.py -t 10
+### 1회 ~ 1007회 Top 10 전체 통계 ###
+43번 : 180개
+1번 : 172개
+27번 : 172개
+34번 : 171개
+17번 : 171개
+13번 : 171개
+39번 : 167개
+4번 : 167개
+33번 : 167개
+12번 : 165개
+```
+6. 당첨번호 Top 보너스볼 제외 통계 확인
+```
+$ python lotto_data.py -t -exb
+### 1회 ~ 1007회 Top 6 보너스볼 제외 통계 ###
+34번 : 152개
+18번 : 148개
+27번 : 146개
+43번 : 146개
+17번 : 145개
+13번 : 145개
+
+$ python lotto_data.py -t 10 -exb
+### 1회 ~ 1007회 Top 10 보너스볼 제외 통계 ###
+34번 : 152개
+18번 : 148개
+27번 : 146개
+43번 : 146개
+17번 : 145개
+13번 : 145개
+39번 : 145개
+1번 : 143개
+12번 : 142개
+14번 : 142개
+```
+7. 당첨번호 커스텀 Top 통계 확인
+```
+$ python lotto_data.py -c -sr 1000 -er 1007
+### 1000회 ~ 1007회 Top 6 전체 통계 ###
+8번 : 4개
+15번 : 4개
+39번 : 3개
+17번 : 3개
+19번 : 2개
+42번 : 2개
+
+$ python lotto_data.py -c 10 -sr 1000 -er 1007
+### 1000회 ~ 1007회 Top 10 전체 통계 ###
+8번 : 4개
+15번 : 4개
+39번 : 3개
+17번 : 3개
+19번 : 2개
+42번 : 2개
+25번 : 2개
+45번 : 2개
+29번 : 2개
+37번 : 2개
+```
+8.당첨번호 커스텀 Top 보너스볼 제외 통계 확인
+```
+$ python lotto_data.py -c -sr 1000 -er 1007 -exb
+### 1000회 ~ 1007회 Top 6 보너스볼 제외 통계 ###
+8번 : 4개
+19번 : 2개
+42번 : 2개
+17번 : 2개
+25번 : 2개
+45번 : 2개
+
+$ python lotto_data.py -c 10 -sr 1000 -er 1007 -exb
+### 1000회 ~ 1007회 Top 10 보너스볼 제외 통계 ###
+8번 : 4개
+19번 : 2개
+42번 : 2개
+17번 : 2개
+25번 : 2개
+45번 : 2개
+29번 : 2개
+39번 : 2개
+15번 : 2개
+37번 : 2개
+```
 ## 현재 데이터 
 - 1회 ~ 1004회 / 2022.03.04 추가
 - 1005회 / 2022.03.05 추가
@@ -34,12 +186,3 @@
 |  bonus  | INTEGER  | 보너스 번호 |
 
 ![table_info](https://user-images.githubusercontent.com/24468970/156866295-02558131-2840-404d-9f56-0cb995c2d0f3.png)
-
-## 최신 당첨 번호 Tool 이용해서 업데이트 하기
-### 사용 방법
-1. python 3.5.x 버전 이상에서 구동
-2. requirements.txt 내 패키기 설치
-   - requests
-   - bs4
-3. lotto_data.py 실행
-   - $ python lotto_data.py
--- lotto_data.py
+++ lotto_data.py
@@ -4,8 +4,23 @@ import sys
 import requests
 import sqlite3
 import os.path
+import argparse
 from bs4 import BeautifulSoup
 from collections import defaultdict
+from collections import Counter
+
+
+class HandelColors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
 
 
 class HandelSQLite:
@@ -30,6 +45,16 @@ class HandelSQLite:
             print(err)
             return False
 
+    def latest_round(self):
+        try:
+            cur = self.conn.cursor()
+            cur.execute('SELECT round FROM tb_lotto_list ORDER BY round DESC')
+            ret = cur.fetchone()[0]
+            return int(ret)
+        except Exception as err:
+            print(err)
+            return 0
+
     def insert_data(self, input_data):
         try:
             cur = self.conn.cursor()
@@ -51,6 +76,86 @@ class HandelSQLite:
             print(err)
             return False
 
+    def select_all(self):
+        try:
+            cur = self.conn.cursor()
+            query = """
+            SELECT 
+                GROUP_CONCAT(data.num) AS num 
+            FROM (
+                SELECT 
+                    ("1st"||","||"2nd"||","||"3rd"||","||"4th"||","||"5th"||","||"6th"||","||"bonus") AS num
+                FROM
+                    tb_lotto_list
+                ) AS data
+            """
+            cur.execute(query)
+            return cur.fetchone()
+        except Exception as err:
+            print(err)
+            return ""
+
+    def select_all_except_bonus(self):
+        try:
+            cur = self.conn.cursor()
+            query = """
+            SELECT 
+                GROUP_CONCAT(data.num) AS num 
+            FROM (
+                SELECT 
+                    ("1st"||","||"2nd"||","||"3rd"||","||"4th"||","||"5th"||","||"6th") AS num
+                FROM
+                    tb_lotto_list
+                ) AS data
+            """
+            cur.execute(query)
+            return cur.fetchone()
+        except Exception as err:
+            print(err)
+            return ""
+
+    def select_custom(self, sround, eround):
+        try:
+            cur = self.conn.cursor()
+            query = """
+            SELECT
+                GROUP_CONCAT(num) AS num 
+            FROM (
+                SELECT 
+                    ("1st"||","||"2nd"||","||"3rd"||","||"4th"||","||"5th"||","||"6th"||","||"bonus") AS num
+                FROM
+                    tb_lotto_list
+                WHERE
+                    round BETWEEN {sround} and {eround}
+                ) 
+            """.format(sround=sround, eround=eround)
+            cur.execute(query)
+            return cur.fetchone()
+        except Exception as err:
+            print(err)
+            return ""
+
+    def select_custom_except_bonus(self, sround, eround):
+        try:
+            cur = self.conn.cursor()
+            query = """
+            SELECT
+                GROUP_CONCAT(num) AS num 
+            FROM (
+                SELECT 
+                    ("1st"||","||"2nd"||","||"3rd"||","||"4th"||","||"5th"||","||"6th") AS num
+                FROM
+                    tb_lotto_list
+                WHERE
+                    round BETWEEN {sround} and {eround}
+                ) 
+            """.format(sround=sround, eround=eround)
+            cur.execute(query)
+            return cur.fetchone()
+        except Exception as err:
+            print(err)
+            return ""
+
 
 class LottoData:
     """
@@ -58,7 +163,7 @@ class LottoData:
     """
     def __init__(self, db_path):
         self.db_path = db_path
-        self.site_url = 'https://www.dhlottery.co.kr/common.do?method=main'
+        self.site_url = 'https://www.dhlottery.co.kr/common.do?method=main&mainMode=default'
 
     def __get_lotto_info(self):
         """
@@ -108,13 +213,121 @@ class LottoData:
                 print("{latest_round}회 로또 당첨 번호 입력 중 문제가 발생하였습니다.".format(latest_round=latest_round))
                 sys.exit(0)
             print("정상적으로 {latest_round}회 로또 당첨 번호를 데이터베이스에 등록하였습니다.".format(latest_round=latest_round))
-            sys.exit(0)
         except Exception as err:
             print(err)
-            sys.exit(0)
+        sys.exit(0)
+
+
+class LottoStatistics:
+    """
+    로또 당첨 번호 통계 확인
+    """
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.handler = HandelSQLite(self.db_path)
+
+    def all(self, bonus=True):
+        try:
+            last_round = self.handler.latest_round()
+            if bonus:
+                data = self.handler.select_all()
+                comment = "전체 통계"
+            else:
+                data = self.handler.select_all_except_bonus()
+                comment = "보너스볼 제외 통계"
+            data_list = list(map(int, data[0].split(",")))
+            counter = Counter(data_list)
+            print("### 1회 ~ {last_round}회 {comment} ###".format(last_round=last_round, comment=comment))
+            for i in counter.most_common(45):
+                print("{num}번 : {cnt}개".format(num=i[0], cnt=i[1]))
+        except Exception as err:
+            print(err)
+        sys.exit(0)
+
+    def custom_top(self, count=6, bonus=True):
+        try:
+            if not isinstance(count, int):
+                count = 6
+            last_round = self.handler.latest_round()
+            if bonus:
+                data = self.handler.select_all()
+                comment = "전체 통계"
+            else:
+                data = self.handler.select_all_except_bonus()
+                comment = "보너스볼 제외 통계"
+            data_list = list(map(int, data[0].split(",")))
+            counter = Counter(data_list)
+            print("### 1회 ~ {last_round}회 Top {count} {comment} ###".format(
+                last_round=last_round, count=count, comment=comment)
+            )
+            for i in counter.most_common(count):
+                print("{num}번 : {cnt}개".format(num=i[0], cnt=i[1]))
+        except Exception as err:
+            print(err)
+        sys.exit(0)
+
+    def custom_round_top(self, sround, eround, count=6, bonus=True):
+        try:
+            last_round = self.handler.latest_round()
+            if not isinstance(sround, int):
+                sround = 1
+            if not isinstance(eround, int):
+                eround = last_round
+            if not isinstance(count, int):
+                count = 6
+            if sround > last_round or eround > last_round:
+                print("입력하신 {eround}회차는 존재하지 않습니다. \n"
+                      "마지막 회차는 {last_round}회차 입니다. \n"
+                      "다시 입력 부탁 드립니다.".format(eround=eround, last_round=last_round))
+                sys.exit(0)
+            if bonus:
+                data = self.handler.select_custom(sround, eround)
+                comment = "전체 통계"
+            else:
+                data = self.handler.select_custom_except_bonus(sround, eround)
+                comment = "보너스볼 제외 통계"
+            data_list = list(map(int, data[0].split(",")))
+            counter = Counter(data_list)
+            print("### {sround}회 ~ {eround}회 Top {count} {comment} ###".format(
+                sround=sround, eround=eround, count=count, comment=comment)
+            )
+            for i in counter.most_common(count):
+                print("{num}번 : {cnt}개".format(num=i[0], cnt=i[1]))
+        except Exception as err:
+            print(err)
+        sys.exit(0)
 
 
 if __name__ == '__main__':
-    db_path = './lotto_data.db'   # 반드시 데이테베이스 위치를 지정.
-    lotto_data = LottoData(db_path)
-    lotto_data.run()
+    parser = argparse.ArgumentParser(prog='lotto', description='로또 최신 당첨번호 입력 및 통계 Tool')
+    try:
+        parser.add_argument('-d', '--db', dest='db_path', type=str, default='./lotto_data.db',
+                            help='로또 당첨번호 DataBase Path')
+        parser.add_argument('-i', '--insert', action='store_true', help='최신 로또 당첨번호 DataBase Insert')
+        parser.add_argument('-s', '--stat', action='store_true', help='로또 당첨번호 통계')
+        parser.add_argument('-t', '--top', type=int, nargs='?', const=6, help='로또 당첨번호 전체 top 통계')
+        parser.add_argument('-c', '--custom', type=int, nargs='?', const=6, help='로또 당첨번호 Custom top 통계')
+        parser.add_argument('-sr', '--sround', type=int, nargs='?', const=1, help='로또 당첨번호 top 통계')
+        parser.add_argument('-er', '--eround', type=int, nargs='?', const=1007, help='로또 당첨번호 top 통계')
+        parser.add_argument('-exb', '--ex_bonus', dest="ex_bonus", action='store_false', help='보너스볼 제외')
+        parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.1')
+        args = parser.parse_args()
+        db_path = args.db_path
+        if args.insert:
+            lotto_data = LottoData(db_path)
+            lotto_data.run()
+            sys.exit(0)
+        LottoStatistics = LottoStatistics(db_path)
+        if args.stat:
+            LottoStatistics.all(args.ex_bonus)
+            sys.exit(0)
+        if args.top:
+            LottoStatistics.custom_top(args.top, args.ex_bonus)
+            sys.exit(0)
+        if args.custom and args.sround and args.eround:
+            LottoStatistics.custom_round_top(args.sround, args.eround, args.custom, args.ex_bonus)
+            sys.exit(0)
+    except Exception as err:
+        print(err)
+        sys.exit(0)
+
